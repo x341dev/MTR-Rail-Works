8b68756936560efbbeebda26fdeae71dfeb608c5
x341dev <admin@x341.dev>
2026-07-20 19:55:26 +0200

    feat: add Rail Worker wall building action

--- fabric/src/main/java/dev/x341/mrw/mod/data/RailActionType.java
+++ fabric/src/main/java/dev/x341/mrw/mod/data/RailActionType.java
@@ -2,7 +2,8 @@ package dev.x341.mrw.mod.data;
 
 public enum RailActionType {
 	TUNNEL_WALL("gui.mtr.percentage_complete_tunnel_wall", "gui.mtr.rail_action_tunnel_wall", 0xFF666666),
-	BRIDGE_WALL("gui.mrw.percentage_complete_bridge_wall", "gui.mrw.rail_action_bridge_wall", 0xFF666666);
+	BRIDGE_WALL("gui.mrw.percentage_complete_bridge_wall", "gui.mrw.rail_action_bridge_wall", 0xFF666666),
+	RAIL_WORKER_WALLS("gui.mrw.percentage_complete_rail_worker_walls", "gui.mrw.rail_action_rail_worker_walls", 0xFF666666);
 
 	public final String progressTranslationKey;
 	public final String nameTranslationKey;
